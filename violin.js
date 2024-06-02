5d14f45aec96de9c8ea672e016c053d6575c268f
bongyeob2 <bongyeob2@gmail.com>
2024-06-03 03:28:09 +0900

    violin + table

--- violin.js
+++ violin.js
@@ -1,17 +1,39 @@
 class ViolinPlot {
-    constructor(data) {
+    constructor(tooltipId, data) {
         this.data = data;
-        this.margin = { top: 10, right: 30, bottom: 30, left: 40 };
+        this.tooltipId = tooltipId;
+        this.tooltip = d3.select(tooltipId);
+        this.margin = { top: 10, right: 30, bottom: 50, left: 70 };
         this.width = 960 - this.margin.left - this.margin.right;
         this.height = 500 - this.margin.top - this.margin.bottom;
         this.svg = null;
         this.x = null;
         this.y = null;
-        this.tooltip = null;
         this.popperInstance = null;
+        this.keysOrder = [
+            "residual sugar", "density", "chlorides", "fixed acidity", 
+            "volatile acidity", "citric acid", "pH", "free sulfur dioxide", 
+            "total sulfur dioxide", "sulphates", "alcohol"
+        ];
+    }
+
+    initializeTable() {
+        const tableContainer = d3.select("#data-table-container");
+        const table = tableContainer.select("#data-table");
+        const tbody = table.select("tbody");
+
+        tbody.html("");
+
+        this.keysOrder.forEach(key => {
+            const row = tbody.append("tr");
+            row.append("th").text(key);
+            row.append("td").text("");
+        });
     }
 
     initialize() {
+        this.initializeTable();
+
         d3.select("#violin").html("");
         this.svg = d3.select("#violin").append("svg")
             .attr("width", this.width + this.margin.left + this.margin.right)
@@ -19,34 +41,45 @@ class ViolinPlot {
             .append("g")
             .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
 
+        this.tooltip = d3.select(this.tooltipId);
+
         this.x = d3.scaleBand()
             .range([0, this.width])
             .domain([3, 4, 5, 6, 7])
             .padding(0.05);
-        this.svg.append("g")
-            .attr("transform", `translate(0,${this.height})`)
-            .call(d3.axisBottom(this.x));
 
         this.y = d3.scaleLinear()
             .range([this.height, 0])
             .domain([0, 1]);
-        this.svg.append("g")
-            .call(d3.axisLeft(this.y));
 
-        this.tooltip = d3.select("body").append("div")
-            .attr("id", "tooltip")
-            .style("position", "absolute")
-            .style("background", "#f9f9f9")
-            .style("padding", "5px")
-            .style("border", "1px solid #d3d3d3")
-            .style("border-radius", "3px")
-            .style("pointer-events", "none")
-            .style("opacity", 0);
+        this.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", `translate(0,${this.height})`)
+            .call(d3.axisBottom(this.x))
+            .append("text")
+            .attr("class", "axis-label")
+            .attr("x", this.width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("quality");
+
+        this.svg.append("g")
+            .attr("class", "y axis")
+            .call(d3.axisLeft(this.y))
+            .append("text")
+            .attr("class", "axis-label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("free sulfur dioxide / total sulfur dioxide");
     }
 
     drawViolinPlots(selectedQualities) {
         const filteredData = this.data.filter(d => selectedQualities.includes(d.quality));
+
         const dataGrouped = d3.groups(filteredData, d => d.quality);
+
         const histogram = d3.histogram()
             .domain(this.y.domain())
             .thresholds(this.y.ticks(20))
@@ -85,7 +118,7 @@ class ViolinPlot {
             );
 
         const jitterWidth = 50;
-        const points = this.svg.selectAll("indPoints")
+        this.svg.selectAll("indPoints")
             .data(filteredData)
             .enter()
             .append("circle")
@@ -97,12 +130,14 @@ class ViolinPlot {
                 return d3.interpolateRgb("white", baseColor)(d.fs_ratio);
             })
             .on("mouseover", (event, d) => this.showTooltip(event, d))
-            .on("mouseout", () => this.hideTooltip());
+            .on("mouseout", () => this.hideTooltip())
+            .on("click", (event, d) => this.showDataInTable(d));
     }
 
     showTooltip(event, d) {
         this.tooltip
-            .style("opacity", 1)
+            .style("display", "block")
+            .select(".tooltip-inner")
             .html(`Free Sulfur Dioxide: ${d['free sulfur dioxide']}<br>Total Sulfur Dioxide: ${d['total sulfur dioxide']}`);
 
         this.popperInstance = Popper.createPopper(event.target, this.tooltip.node(), {
@@ -117,11 +152,29 @@ class ViolinPlot {
     }
 
     hideTooltip() {
-        this.tooltip.style("opacity", 0);
+        this.tooltip.style("display", "none");
         if (this.popperInstance) {
             this.popperInstance.destroy();
             this.popperInstance = null;
         }
+    }
+
+    showDataInTable(d) {
+        const tableContainer = d3.select("#data-table-container");
+        const table = tableContainer.select("#data-table");
+        const tbody = table.select("tbody");
+
+        tbody.selectAll("tr").select("td").text("");
+
+        this.keysOrder.forEach(key => {
+            tbody.selectAll("tr")
+                .filter(function() { return d3.select(this).select("th").text() === key; })
+                .select("td")
+                .text(() => {
+                    let value = d[key];
+                    return (typeof value === "number" && value.toString().split(".")[1]?.length > 6)? value.toFixed(6): value;
+                });
+        });
     }
 
     update(selectedQualities) {
